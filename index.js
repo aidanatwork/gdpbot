b941fe6a29289ebca374e66551546288cd257d07
Aidan Doherty <aidan@aidanatwork.com>
2020-11-04 13:40:39 -0800

    nearly done with feature to add an issue. getting 401 error trying to authenticate with GitHub

--- index.js
+++ index.js
@@ -11,34 +11,58 @@ const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 const issuesDomain = process.env.GIT_ISSUES_DOMAIN;
 const apiDomain = process.env.GIT_API_DOMAIN;
+const gitUser = process.env.GIT_USER;
+const gitPwd = process.env.GIT_PWD;
 const owner = process.env.GIT_OWNER;
 const repos = process.env.GIT_REPOS;
 const PORT = process.env.PORT;
 const botToken = process.env.SLACK_BOT_TOKEN;
-//TODO - change the regex so it looks for repo name also being specified
-const issueRegex = /issue\s[0-9]+/gim;
+const issueRegex = /\b[\S]+?\b\sissue\s[0-9]+/gim;
+const linkRegex = /\<.+?\>/gim;
+
+const createBasicAuth = function (user,pwd) {
+  let str = user + ':' + pwd;
+  let buff = Buffer.from(str, 'utf-8');
+  let base64 = buff.toString('base64');
+  return base64;
+};
 
 //TODO - automatically add this bot to any new channel that is created
 
 //TODO - check if the link to GitHub is actually valid before adding it to reply
 //create issue link
 const createIssueLink = function(repo, issueNumber) {
-  var newLink = issuesDomain + "/" + owner + "/" + repo + "/issues/" + issueNumber;
-  return newLink;
+  return issuesDomain + "/" + owner + "/" + repo + "/issues/" + issueNumber;
 };
+//strip out links from Slack message text
+const stripOutLinks = function(text) {
+  var new_text = text;
+  if (linkRegex.test(text)) {
+    text.match(linkRegex).forEach(function(item){
+      let edited_item = item.substring(1, item.length - 1);
+      edited_item = edited_item.split("|")[1]
+      new_text = new_text.replace(item,edited_item);
+    });
+  }
+  return new_text;
+};
+
 //create issue link text
 //TODO - change this so the repo name gets pulled out of 'item' and passed to createIssueLink. Maybe map shorthand names to the longer repo names.
 const createIssueLinksText = function (msg_text) {
   let new_msg = '';
   msg_text.match(issueRegex).forEach(function(item) {
-    let issueNumber = item.split(" ")[1];
-    let newLink = createIssueLink('templates', issueNumber);
+    let itemArray = item.split("issue");
+    let issueNumber = itemArray[1].trim();
+    let issueRepo = itemArray[0].trim();
+    let newLink = createIssueLink(issueRepo, issueNumber);
     new_msg += 'See GitHub <' + newLink + '|' + item + '> that was just mentioned.\n';
   });
   return new_msg;
 };
 
 //send reply to issue mention
+//TODO - convert part of match that is hyperlink to just text of the link
 const replyWithIssue = function(msg_text, channel) {
   let new_msg_text = createIssueLinksText(msg_text);
   let new_msg = {
@@ -61,6 +85,9 @@ const replyWithIssue = function(msg_text, channel) {
       console.error(error);
     });
 };
+
+// create auth string for GitHub
+const basicAuth = 'Basic ' + createBasicAuth(gitUser, gitPwd);
 
 // instantiate and configure Express app
 const app = express();
@@ -108,16 +135,42 @@ app.post('/command', function(req, res) {
 });
 
 //Provide a GitHub link when an issue is mentioned with the /issue slash command in a channel.
-/*app.post('/issue', function(req, res) {
-  console.log('/issue command was received');
+app.post('/create', function(req, res) {
+  console.log('/create command was received');
   res.sendStatus(200);
-  var msg = req.body;
-  var msg_text = msg.text;
-  var channel = msg.channel_id;
-  if (issueRegex.test(msg_text)) {
+  let msg = req.body;
+  console.log('msg: ' + JSON.stringify(msg));
+  let msg_text = stripOutLinks(msg.text).trim();
+  let split_msg = msg_text.split("|");
+  let repo = split_msg[0].trim();
+  let title = split_msg[1].trim();
+  let body = split_msg[2].trim();
+  let url = apiDomain + "/repos/" + owner + "/" + repo + "/issues";
+  console.log('url: ' + url);
+  let data = {
+    "title": title,
+    "body": body
+  };
+  let config = {
+    headers: {
+      'Authorization': basicAuth,
+      'Content-Type': 'application/vnd.github.v3+json'
+    }
+  };
+  axios
+    .post(url, data, config)
+    .then(function (response) {
+      console.log('Response:');
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  let channel = msg.channel_id;
+  /*if (issueRegex.test(msg_text)) {
     replyWithIssue(msg_text, channel);
-  }
-});*/
+  }*/
+});
 
 // TODO - fix so this can reply to mentions in private channels. right now only works for public channels.
 // This route listens for "issue" in a message, and if it is followed by a number, brings in the GitHub link for that issue.
@@ -128,7 +181,8 @@ app.post('/message', function(req, res){
   } else {
     let msg = req.body;
     //console.log('msg: ' + JSON.stringify(msg));
-    let msg_text = msg.event.blocks[0].elements[0].elements[0].text;
+    let msg_text = msg.event.text;
+    msg_text = stripOutLinks(msg_text);
     let channel = msg.event.channel;
     if (issueRegex.test(msg_text)) {
       replyWithIssue(msg_text, channel);
